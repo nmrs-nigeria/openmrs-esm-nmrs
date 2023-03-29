e326c5acd5d90fa101323c982a94525f3b7b9c89
Pius Rubangakene <piruville@gmail.com>
2023-03-29 18:39:32 +0300

    NMRS covid and patient chart esm (#4)

---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const caseInvestigationEncounterType_UUID =
+  "0a573847-276b-4a39-80f2-c3463b1716b0";
---
+++ src/dashboard.meta.tsx
@@ -0,0 +1,17 @@
+export const CaseInvestigationMeta = {
+  slot: "case-investigation-slot",
+  columns: 1,
+  title: "Case Investigation",
+};
+
+export const ContactInvestigationMeta = {
+  slot: "contact-investigation-slot",
+  columns: 1,
+  title: "Contact Investigation",
+};
+
+export const DailyMonitoringOfContactMeta = {
+  slot: "daily-monitoring-of-contact-slot",
+  columns: 1,
+  title: "Daily Monitoring of Contact",
+};
