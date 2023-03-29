1b43b12f9f9d296c3c1b10460e88c12ac768bda3
pirupius <piruville@gmail.com>
2023-03-29 09:02:38 +0300

    Node CI setup and clean ups

--- src/nmrs-logo-config.ts
+++ src/nmrs-logo-config.ts
@@ -1,14 +1,15 @@
-import nmrsLogo from './images/logo.png';
+import nmrsLogo from "./images/logo.png";
+import nmrsLogoSmall from "./images/nmrs-logo-sm.png";
 
 export default {
-    '@openmrs/esm-login-app': {
-        logo: {
-            src: nmrsLogo
-        },
+  "@openmrs/esm-login-app": {
+    logo: {
+      src: nmrsLogoSmall,
     },
-    '@openmrs/esm-primary-navigation-app': {
-        logo: {
-            src: nmrsLogo,
-        },
-    }
-}
+  },
+  "@openmrs/esm-primary-navigation-app": {
+    logo: {
+      src: nmrsLogoSmall,
+    },
+  },
+};
