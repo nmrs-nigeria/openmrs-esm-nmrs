081d9bcc831eb0963c2a46edabf5e3671439b8fe
AJAL ODORA JONATHAN <43242517+ODORA0@users.noreply.github.com>
2023-04-03 12:58:18 +0300

    Add daily monitoring to nmrs

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,4 @@
 export const caseInvestigationEncounterType_UUID =
   "0a573847-276b-4a39-80f2-c3463b1716b0";
+export const dailyCaseMonitoringEncounterType_UUID =
+  "25290479-371b-47f8-9ddd-4b87f7c62be2";
