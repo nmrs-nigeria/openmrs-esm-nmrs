b7877b687faff6f4432f6c0cf2781b46ce16ca15
AJAL ODORA JONATHAN <43242517+ODORA0@users.noreply.github.com>
2023-04-06 13:33:26 +0300

    Add contact investigation (#9)
    * Add contact investigation
    
    * Refactor table definition, add translations for column names
    
    * PR comments
    
    * PR comments
    
    * PR comments

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,10 @@ export const caseInvestigationEncounterType_UUID =
   "0a573847-276b-4a39-80f2-c3463b1716b0";
 export const dailyCaseMonitoringEncounterType_UUID =
   "25290479-371b-47f8-9ddd-4b87f7c62be2";
+
+//contactInvesigation
+export const contactInvestigationEncounterType_UUID =
+  "c4b87cd6-8006-49c4-a69a-3a21a85f0937";
+export const prolongedContact_UUID = "b1781ad6-271f-4bad-a9e5-fae8efb8cb60";
+export const typeOfContact_UUID = "11bf2766-7fc2-4927-b417-b15c175c61ee";
+export const contactNumber_UUID = "0fdf0dba-c8b6-41d0-a451-4e15d597776e";
--- src/views/contact-investigation/contact-investigation.tsx
+++ src/views/contact-investigation/contact-investigation.tsx
@@ -1,15 +1,83 @@
-import React from "react";
+import React, { useMemo } from "react";
 import {
-  EmptyStateComingSoon,
+  EncounterList,
+  EncounterListColumn,
+  getObsFromEncounter,
   PatientChartProps,
 } from "@ohri/openmrs-esm-ohri-commons-lib";
 import { useTranslation } from "react-i18next";
+import { moduleName } from "../..";
+import {
+  contactInvestigationEncounterType_UUID,
+  contactNumber_UUID,
+  prolongedContact_UUID,
+  typeOfContact_UUID,
+} from "../../constants";
 
 const ContactInvestigation: React.FC<PatientChartProps> = ({ patientUuid }) => {
   const { t } = useTranslation();
   const title = t("contactInvestigation", "Contact Investigation");
 
-  return <EmptyStateComingSoon headerTitle={title} displayText={title} />;
+  const columns: EncounterListColumn[] = useMemo(
+    () => [
+      {
+        key: "days",
+        header: t("contactIdNumber", "Contact ID number"),
+        getValue: (encounter) => {
+          return getObsFromEncounter(encounter, contactNumber_UUID);
+        },
+      },
+      {
+        key: "lossOfSmell",
+        header: t("prolongedContact", "Prolonged Contact"),
+        getValue: (encounter) => {
+          return getObsFromEncounter(encounter, prolongedContact_UUID);
+        },
+      },
+      {
+        key: "shortnessOfBreath:",
+        header: t("typeOfContact", "Type of Contact"),
+        getValue: (encounter) => {
+          return getObsFromEncounter(encounter, typeOfContact_UUID);
+        },
+      },
+      {
+        key: "actions",
+        header: t("actions", "Actions"),
+        getValue: (encounter) => [
+          {
+            form: { name: "covid", package: "contact_investigation" },
+            encounterUuid: encounter.uuid,
+            intent: "*",
+            label: t("viewDetails", "View Details"),
+            mode: "view",
+          },
+          {
+            form: { name: "covid", package: "contact_investigation" },
+            encounterUuid: encounter.uuid,
+            intent: "*",
+            label: t("editForm", "Edit Form"),
+            mode: "edit",
+          },
+        ],
+      },
+    ],
+    []
+  );
+  return (
+    <EncounterList
+      patientUuid={patientUuid}
+      encounterUuid={contactInvestigationEncounterType_UUID}
+      form={{ package: "covid", name: "contact_investigation" }}
+      columns={columns}
+      description={title}
+      headerTitle={title}
+      launchOptions={{
+        displayText: t("add", "Add"),
+        moduleName: moduleName,
+      }}
+    />
+  );
 };
 
 export default ContactInvestigation;
